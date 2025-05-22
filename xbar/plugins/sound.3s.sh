2a817539c319c14c077187b170b35ae0bbac8ae0
Daichi Hioki <hioki.daichi@gmail.com>
2025-05-22 14:46:11 +0900

    音声入力と出力の状態を表示するスクリプトを更新

--- xbar/plugins/sound.3s.sh
+++ xbar/plugins/sound.3s.sh
@@ -1,3 +1,26 @@
 #!/usr/bin/env bash
 
-echo "$(/opt/homebrew/bin/SwitchAudioSource -c -t input)/$(/opt/homebrew/bin/SwitchAudioSource -c -t output)"
+
+input=$(/opt/homebrew/bin/SwitchAudioSource -c -t input)
+output=$(/opt/homebrew/bin/SwitchAudioSource -c -t output)
+
+PC="💻️"
+AIRPODS="ᖰ ᖳ"
+
+if [[ $input == "外部マイク" ]]; then
+  input="🎤"
+elif [[ $input == "MacBook Proのマイク" ]]; then
+  input=$PC
+elif [[ $input =~ "AirPods" ]]; then
+  input=$AIRPODS
+fi
+
+if [[ $output == "外部ヘッドフォン" ]]; then
+  output="🎧️"
+elif [[ $output == "MacBook Proのスピーカー" ]]; then
+  output=$PC
+elif [[ $output =~ "AirPods" ]]; then
+  output=$AIRPODS
+fi
+
+echo "$input/$output"
