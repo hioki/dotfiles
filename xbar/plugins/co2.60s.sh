43516d7aa333f7e619769e84c96ede2539991ee5
Daichi Hioki <hioki.daichi@gmail.com>
2025-05-27 13:27:57 +0900

    Update co2.60s.sh

--- xbar/plugins/co2.60s.sh
+++ xbar/plugins/co2.60s.sh
@@ -1,9 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 source ".switchbot.env"
-CO2=$(curl -s \
+json=$(curl -s \
   -H "Authorization: $SWITCHBOT_PRIVATE_ACCOUNT_TOKEN" \
   -H "Content-Type: application/json" \
-  "https://api.switch-bot.com/v1.1/devices/$SWITCHBOT_CO2_DEVICE_ID/status" \
-| jq '.body.CO2')
-echo "${CO2} ppm"
+  "https://api.switch-bot.com/v1.1/devices/$SWITCHBOT_CO2_DEVICE_ID/status")
+co2=$(echo "$json" | jq -r '.body.CO2')
+
+if   [[ $co2 -ge 1500 ]]; then  color="#FF9999"
+elif [[ $co2 -ge 1000 ]]; then  color="#FFDD99"
+else                            color="#BBFFBB"
+fi
+
+echo "${co2} ppm | color=${color}"
+
+# --- 以下はクリックで開くドロップダウン（任意） -------
+temp=$(echo "$json" | jq -r '.body.temperature')
+humi=$(echo "$json" | jq -r '.body.humidity')
+echo "---"
+echo "温度: ${temp} ℃"
+echo "湿度: ${humi} %"
+echo "更新: $(date '+%Y-%m-%d %H:%M:%S')"
+echo "Refresh | refresh=true"
