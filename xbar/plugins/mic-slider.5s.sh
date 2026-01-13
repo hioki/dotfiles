92463d6681fd6ce81fdca2cf91213191d11a0399
Daichi Hioki <hioki.daichi@gmail.com>
2026-01-13 22:57:50 +0900

    Update mic-slider.5s.sh

--- xbar/plugins/mic-slider.5s.sh
+++ xbar/plugins/mic-slider.5s.sh
@@ -3,7 +3,7 @@
 # <xbar.version>v1.0</xbar.version>
 # <xbar.author>hioki</xbar.author>
 # <xbar.desc>Displays the macOS System Input Volume slider value.</xbar.desc>
-# <xbar.refresh>1s</xbar.refresh>
+# <xbar.refresh>5s</xbar.refresh>
 
 # AppleScript で「入力音量スライダー」の現在値を取得（0～100 の整数）
 VOL=$(osascript -e 'input volume of (get volume settings)')
